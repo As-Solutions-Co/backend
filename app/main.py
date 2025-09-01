6d48f203b30707064117084529babb121d41b083
Juan Diego <95363339+Juandiegogb@users.noreply.github.com>
2025-09-01 12:39:36 -0500

    Create api router for api.
    Grouping all api endpoints in one ApiRouter = api_router

--- app/main.py
+++ app/main.py
@@ -1,13 +1,8 @@
 from fastapi import FastAPI
 from app.core.config import settings
+from app.api.main import api_router
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {
-        "Hello": str(settings.SQLALCHEMY_DATABASE_URI),
-        "type": str(type(settings.SQLALCHEMY_DATABASE_URI)),
-        "message": "Archivo escrito correctamente 🚀",
-    }
+app.include_router(api_router)
