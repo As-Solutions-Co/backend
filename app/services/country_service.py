5e8f753014fde8da126059858c8acb6f85f8e8b1
Juan Diego <95363339+Juandiegogb@users.noreply.github.com>
2025-09-04 09:36:56 -0500

    Country modifications

--- app/services/country_service.py
+++ app/services/country_service.py
@@ -1,23 +1,29 @@
-from app.crud.country_crud import get_all_countries, get_country_by_id
-from app.models.country import CountriesPublic, Country
-from sqlmodel import Session
 from uuid import UUID
+
+from fastapi import HTTPException, status
+from sqlmodel import Session
+
+from app.crud.country_crud import (
+    get_all_countries,
+    get_country_by_id,
+    get_country_by_name,
+)
+from app.models.country import CountriesPublic, Country
 
 
 def get_countries_service(
-    session: Session, skip: int = 0, limit: int = 10
+    session: Session, skip: int = 0, limit: int = 10, name: str = None
 ) -> CountriesPublic:
-    from sqlmodel import select
-
-    total = len(session.exec(select(Country)).all())
-    countries = get_all_countries(session, skip=skip, limit=limit)
+    if name:
+        countries = get_country_by_name(session, name)
+    else:
+        countries = get_all_countries(session, skip=skip, limit=limit)
+    total = len(countries)
     return CountriesPublic(data=countries, count=total)
 
 
 def get_country_service(session: Session, country_id: UUID) -> Country:
     country = get_country_by_id(session, country_id)
     if not country:
-        from fastapi import HTTPException, status
-
         raise HTTPException(status.HTTP_404_NOT_FOUND, "Country not found")
     return country
