50d4851ad192d97ca722d4c633b5e66d2930a0c2
Juan Diego <95363339+Juandiegogb@users.noreply.github.com>
2025-09-05 22:51:13 -0500

    Simplify app_user create

--- app/crud/app_user_crud.py
+++ app/crud/app_user_crud.py
@@ -3,14 +3,8 @@ from sqlmodel import Session
 from app.models import AppUserRegistration, AppUser
 
 
-def create_admin_app_user(
-    session: Session, user_data: AppUserRegistration, organization_id: UUID
-):
-    app_user = AppUser(
-        **user_data.model_dump(),
-        user_type_id="3e1208bc-aa8d-42d7-b957-95895a2f60aa",
-        organization_id=organization_id,
-    )
+def create_app_user(session: Session, user_data: AppUser):
+    app_user = AppUser(**user_data.model_dump())
     session.add(app_user)
     session.flush()
     session.refresh(app_user)
