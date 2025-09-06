d6687f52c3de44d173b2c097334f1bf81c288a3e
Juan Diego <95363339+Juandiegogb@users.noreply.github.com>
2025-09-05 22:52:19 -0500

    Enhance register service

---
+++ app/schemas/auth_schema.py
@@ -0,0 +1,7 @@
+from pydantic import BaseModel
+from app.models import AppUserRegistration, OrganizationCreate
+
+
+class RegisterRequest(BaseModel):
+    user_data: AppUserRegistration
+    organization_data: OrganizationCreate
--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -1,22 +1,59 @@
+from uuid import UUID
 from sqlmodel import Session
-from app.models import AppUserRegistration, OrganizationCreate
-from app.services.document_type_service import get_document_type_by_id_service
-from app.crud.organization_crud import create_organization
-from app.crud.app_user_crud import create_admin_app_user
+from sqlalchemy.exc import SQLAlchemyError
+from fastapi import HTTPException, status
+
+from app.crud.document_type_crud import read_document_type_by_id
+from app.crud.user_type import read_user_type_by_name
+from app.crud.organization_crud import create_organization, read_organization_by_name
+from app.crud.app_user_crud import create_app_user
+from app.models.app_user_model import AppUser
+from app.models import Organization, AppUser as AppUserModel
+from app.schemas.auth_schema import RegisterRequest
+from app.utils.security import hash_password
 
 
-def post_user_organization_service(
-    session: Session,
-    user_data: AppUserRegistration,
-    organization_data: OrganizationCreate,
-):
+def register_service(
+    session: Session, register_data: RegisterRequest
+) -> dict[str, Organization | AppUserModel]:
+    organization_data = register_data.organization_data
+    user_data = register_data.user_data
+
+    if read_organization_by_name(session, organization_data.name):
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="An organization with this name already exists.",
+        )
+
+    document_type = read_document_type_by_id(session, user_data.document_type_id)
+    if not document_type:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Document type with id {user_data.document_type_id} not found",
+        )
+
+    admin_user_type = read_user_type_by_name(session, name="admin")
+
     try:
-        organization = create_organization(session, organization_data)
-        admin_user = create_admin_app_user(session, user_data, organization.id)
+        new_organization = create_organization(session, organization_data)
+        session.flush()
+
+        user_data_extended = AppUser(
+            **user_data.model_dump(),
+            organization_id=new_organization.id,
+            user_type_id=admin_user_type.id,
+        )
+
+        user_data_extended.password = hash_password(user_data_extended.password)
+
+        admin_user = create_app_user(session, user_data_extended)
+
         session.commit()
-        session.refresh(organization)
+        session.refresh(new_organization)
         session.refresh(admin_user)
-        return organization, admin_user
-    except Exception:
+
+        return {"organization": new_organization, "admin_user": admin_user}
+
+    except SQLAlchemyError:
         session.rollback()
         raise
