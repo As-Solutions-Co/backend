890f8ed9880f1f5aa52e8e94245bbe3ff5a708fb
Juan Diego <95363339+Juandiegogb@users.noreply.github.com>
2025-09-06 00:49:57 -0500

    Split MixinBase model, add IdMixin

--- app/models/mixins.py
+++ app/models/mixins.py
@@ -4,8 +4,11 @@ from uuid import UUID, uuid4
 from datetime import datetime
 
 
-class MixinBase(SQLModel):
+class IdMixin(SQLModel):
     id: UUID = Field(primary_key=True, default_factory=uuid4)
+
+
+class MixinBase(IdMixin):
     created_at: datetime = Field(
         sa_column_kwargs={"server_default": func.now()},
         nullable=False,
