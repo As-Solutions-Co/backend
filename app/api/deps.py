27db38100ee7ebd00ba1c2fc12f365886ed3bd12
Juan Diego <95363339+Juandiegogb@users.noreply.github.com>
2025-09-08 19:09:20 -0500

    Add feature to currentUserDep, check invalid_token table

--- app/api/deps.py
+++ app/api/deps.py
@@ -4,13 +4,14 @@ from typing import Annotated
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from jwt.exceptions import InvalidTokenError
-from sqlmodel import Session
+from sqlmodel import Session, select
 
 from app.core.config import settings
 from app.core.db import engine
 from app.core.security import check_token
 from app.crud.app_user_crud import read_app_user_by_id
 from app.models.app_user_model import AppUser
+from app.models.invalid_token_model import InvalidToken
 
 
 def get_db() -> Generator[Session]:
@@ -27,6 +28,13 @@ TokenDep = Annotated[str, Depends(oauth_schema)]
 
 def get_current_user(session: SessionDep, token: TokenDep) -> AppUser:
     try:
+        invalid_token_exists = session.exec(
+            select(InvalidToken).where(InvalidToken.token == token)
+        ).first()
+
+        if invalid_token_exists:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+
         payload = check_token(token)
     except InvalidTokenError:
         raise HTTPException(
