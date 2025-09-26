c6fc73aa1668cf5123050f14567b822db57f914a
Juan Diego <95363339+Juandiegogb@users.noreply.github.com>
2025-09-26 03:43:03 -0500

    Organization service done

--- src/resources/organizations/crud.py
+++ src/resources/organizations/crud.py
@@ -1,11 +1,17 @@
-import json
-from sqlmodel import select, Session
-from models import Organization
-from db import engine
 from uuid import UUID
+
+from db import engine
+from models import Organization
+from sqlmodel import Session, select
 
 
 def read_organization():
     with Session(engine) as session:
         stmt = select(Organization)
         return session.exec(stmt).all()
+
+
+def read_organization_by_id(id: UUID):
+    with Session(engine) as session:
+        stmt = select(Organization).where(Organization.id == id)
+        return session.exec(stmt).first()
