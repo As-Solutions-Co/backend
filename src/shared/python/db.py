cc9359a0ffc3171e047bd26f67ccbb25c3e44978
Juan Diego <95363339+Juandiegogb@users.noreply.github.com>
2025-09-26 00:59:45 -0500

    Refactor code

---
+++ src/shared/python/db.py
@@ -0,0 +1,25 @@
+import json
+from sqlmodel import create_engine, Session
+import os
+import boto3
+from botocore.exceptions import ClientError
+
+
+secret_id = os.getenv("SECRET_ARN")
+region_name = "us-east-1"
+session = boto3.session.Session()
+client = session.client(service_name="secretsmanager", region_name=region_name)
+
+try:
+    get_secret_value_response = client.get_secret_value(SecretId=secret_id)
+    secret = json.loads(get_secret_value_response["SecretString"])
+    USERNAME = secret["username"]
+    PASSWORD = secret["password"]
+    HOST = secret["host"]
+    PORT = secret["port"]
+    DB_NAME = os.getenv("DB_NAME")
+    DB_URL = f"postgresql+psycopg2://{USERNAME}:{PASSWORD}@{HOST}:{PORT}"
+    engine = create_engine(DB_URL)
+
+except (ClientError, KeyError, ValueError) as e:
+    raise e
