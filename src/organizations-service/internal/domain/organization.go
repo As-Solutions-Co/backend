90d0f5bb158be283ca722387305259c66c1f36e0
Juan Diego <95363339+Juandiegogb@users.noreply.github.com>
2025-10-27 17:04:10 -0500

    New validate function in organization entity

--- src/organizations-service/internal/domain/organization.go
+++ src/organizations-service/internal/domain/organization.go
@@ -1,7 +1,16 @@
 package domain
 
+import (
+	"github.com/go-playground/validator/v10"
+)
+
 type Organization struct {
 	Id        interface{} `json:"id"`
-	Name      string      `json:"name"`
-	MainColor string      `json:"main_color"`
+	Name      string      `json:"name" validate:"required"`
+	MainColor string      `json:"main_color" validate:"required"`
+}
+
+func (o Organization) Validate() error {
+	v := validator.New()
+	return v.Struct(o)
 }
