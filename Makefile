3e03a4e8fdbaab3020077341126ca1bd583d25d2
Juan Diego <95363339+Juandiegogb@users.noreply.github.com>
2025-10-27 19:13:41 -0500

    Migration cdk ts to golang

--- Makefile
+++
@@ -1,3 +0,0 @@
-test:
-	cdk synth
-	sam local start-api -t cdk.out/BackendStack.template.json -n env.json --docker-network aws_network --warm-containers eager
--- bin/backend.ts
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-import * as cdk from "aws-cdk-lib";
-import { BackendStack } from "../lib/backend-stack";
-
-const app = new cdk.App();
-new BackendStack(app, "BackendStack", {
-  env: { account: "057149785827", region: "us-east-1" },
-});
